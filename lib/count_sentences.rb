7b6fad35e662887a48809f3df4c14761ada09763
vy vo <tuonggvy.vo@gmail.com>
2020-12-01 04:23:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,23 +1,47 @@
 require 'pry'
 
-class String
+# class String
 
+#   def sentence?
+#     self.end_with?(".")
+#   end
+
+#   def question?
+#     self.end_with?("?")
+#   end
+
+#   def exclamation?
+#     self.end_with?("!")
+#   end
+  
+#   def count_sentences
+#     array = self.split(/[.?!]/)
+#     array.delete_if{|sentence| sentence.empty?}
+#     array.length
+#   end
+# end
+
+class String
+  
+  attr_accessor :string
+  
   def sentence?
     self.end_with?(".")
   end
-
+  
   def question?
     self.end_with?("?")
   end
-
+  
   def exclamation?
     self.end_with?("!")
   end
   
+  #each sentence separate by a . ? !. Split them up and add to an array
+  #
+  
   def count_sentences
-    array = self.split(/[.?!]/)
-    array.delete_if{|sentence| sentence.empty?}
-    array.length
+    
   end
+  
 end
-
